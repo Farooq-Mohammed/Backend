def73decd2fe18104b3eba9b782839bd1509f9cd
Farooq-Mohammed <omar.farooq023@gmail.com>
2023-12-23 00:13:38 +0530

    used type:module instead of commonjs

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const app = express();
+import express from "express";
+import dotenv from "dotenv";
 
+const app = express();
 dotenv.config();
-const port = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.send("Server is ready");
 });
 
-app.get("/login", (req, res) => {
-	res.send("<h1>Login</h1>");
+app.get("/api/jokes", (req, res) => {
+	const jokes = [
+		{ id: 1, title: "First one", content: "Joke One" },
+		{ id: 2, title: "Second one", content: "Joke Two" },
+		{ id: 3, title: "Third one", content: "Joke Three" },
+		{ id: 4, title: "Fourth one", content: "Joke Four" },
+		{ id: 5, title: "Fifth one", content: "Joke Five" },
+	];
+	res.send(jokes);
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
